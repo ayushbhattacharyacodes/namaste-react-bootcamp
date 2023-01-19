34fcfb6c878862f7d1af63555795d8ca95a7c357
Ayush Bhattacharya <iamayush83@gmail.com>
2023-01-19 14:47:00 +0530

    added authentication button

--- Day-6-to-9/src/components/Header/Navs.js
+++ Day-6-to-9/src/components/Header/Navs.js
@@ -1,24 +1,42 @@
-import "./Navs.css"
+import { useState } from "react";
+import "./Navs.css";
 
 const Navs = () => {
-    return (
-      <div className="navs">
-        <ul>
-          <li>
-            <a href="/">Home</a>
-          </li>
-          <li>
-            <a>About</a>
-          </li>
-          <li>
-            <a>Contact</a>
-          </li>
-          <li>
-            <a>Cart</a>
-          </li>
-        </ul>
-      </div>
-    );
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const toggleLogin = () => {
+    setIsLoggedIn(!isLoggedIn);
   };
 
-  export default Navs;
+  return (
+    <div className="navs">
+      <ul>
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a>About</a>
+        </li>
+        <li>
+          <a>Contact</a>
+        </li>
+        <li>
+          <a>Cart</a>
+        </li>
+        <li>
+          {!isLoggedIn && (
+            <button className="auth-btn" onClick={toggleLogin}>
+              Logout
+            </button>
+          )}
+          {isLoggedIn && (
+            <button className="auth-btn" onClick={toggleLogin}>
+              Login
+            </button>
+          )}
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navs;
